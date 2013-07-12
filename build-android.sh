eafe6b86104008b5711649167f1a6aef48ccde56
Charley Robinson <charley@tokbox.com>
2013-07-12 06:47:21 -0700

    dirname hell

--- build-android.sh
+++ build-android.sh
@@ -5,9 +5,10 @@ gclient config https://webrtc.googlecode.com/svn/trunk
 echo "target_os = ['android', 'unix']" >> .gclient
 gclient sync --nohooks
 
-cd trunk
-. ./build/android/envsetup_functions.sh
-. ./build/android/envsetup.sh
+cd trunk/build/android
+. ./envsetup_functions.sh
+. ./envsetup.sh
+cd ../..
 gclient runhooks
 ninja -C out/Debug -t targets
 ninja -C out/Debug video_demo_apk
